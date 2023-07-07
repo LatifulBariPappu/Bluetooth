a5c3c3207fc14fbb309e8470a8e7fc9ee566a681
LatifulBari <latifulbari2001@gmail.com>
2023-07-07 17:13:51 +0600

    day3

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -1,29 +1,92 @@
 package com.example.bluetooth;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT=0;
-    private static final int REQUEST_DISCOVER_BT=1;
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
 
-    private TextView mStatusBleTv,mPairedTv;
+    private TextView mStatusBleTv, mPairedTv;
     ImageView mBlueTv;
-    Button mOnBtn,mOffBtn,mDiscoveredBtn,mPairedBtn;
+    Button mOnBtn, mOffBtn, mDiscoveredBtn, mPairedBtn;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mStatusBleTv=findViewById(R.id.statusBlutoothTv);
-        mPairedTv=findViewById(R.id.pairTv);
-        mBlueTv=findViewById(R.id.bluetoothTv);
+        mStatusBleTv = findViewById(R.id.statusBlutoothTv);
+        mPairedTv = findViewById(R.id.pairTv);
+        mBlueTv = findViewById(R.id.bluetoothTv);
+        mOnBtn = findViewById(R.id.onBtn);
+        mOffBtn = findViewById(R.id.offBtn);
+        mDiscoveredBtn = findViewById(R.id.discoverableBtn);
+        mPairedBtn = findViewById(R.id.pairedBtn);
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        if (bluetoothAdapter == null) {
+            mStatusBleTv.setText("Bluetooth is not available");
+        } else {
+            mStatusBleTv.setText("Bluetooth is available");
+        }
+        if (bluetoothAdapter.isEnabled()) {
+            mBlueTv.setImageResource(R.drawable.on);
+        } else {
+            mBlueTv.setImageResource(R.drawable.off);
+        }
 
+        mOnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bluetoothAdapter.isEnabled()) {
+                    showToast("Turning on Bluetooth");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+                } else {
+                    showToast("Bluetooth is already on");
+                }
+            }
+        });
+
+        mDiscoveredBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+            }if (bluetoothAdapter.isDiscovering()){
+                showToast("Making your devixe discoverable");
+                Intent intent=new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                startActivityForResult(intent,REQUEST_DISCOVER_BT);
+            }
+        });
+        mOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bluetoothAdapter.isEnabled()){
+                    bluetoothAdapter.disable();
+                    showToast("Turning bluetooth off");
+                    mBlueTv.setImageResource(R.drawable.off);
+                }else{
+                    showToast("Blutooth is already off");
+                }
+            }
+        });
+
+    }
+    private void showToast(String msg){
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
     }
 }
